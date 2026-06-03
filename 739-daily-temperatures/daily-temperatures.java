17ec8094745801ef9731061e4b69dfc4b85642b4
Ankit <ankitdey2004@gmail.com>
2026-06-03 12:39:02 +0530

    Time: 71 ms (36.71%) | Memory: 107.8 MB (14.92%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        int n = temperatures.length;
+        int curr[] = new int[n];
+        
+        for(int i = 1 ; i < n ; i++) {
+            while(!st.empty() && temperatures[i] > temperatures[st.peek()])
+                curr[st.peek()] = i - st.pop();
+            st.push(i);
+        }
+
+        return curr;
+    }
+}
