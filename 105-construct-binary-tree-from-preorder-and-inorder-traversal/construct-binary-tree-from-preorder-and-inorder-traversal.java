01f577a7905f7e56a11faf524a2d393ba91cfdd2
Ankit <ankitdey2004@gmail.com>
2026-06-29 11:38:18 +0530

    Time: 2 ms (64.04%) | Memory: 46.5 MB (36.53%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,21 +14,25 @@
  * }
  */
 class Solution {
+    int ind = 0;
+    HashMap<Integer, Integer> mp = new HashMap<>();
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length == 0 || inorder.length == 0)
+        for(int i = 0 ; i < inorder.length ; i++)
+            mp.put(inorder[i], i);
+        
+        return dfs(preorder, 0, inorder.length - 1);
+    }
+
+    TreeNode dfs(int preorder[], int l, int r) {
+        if(l > r)
             return null;
         
-        int x = preorder[0];
-        int pl = preorder.length, il = inorder.length;
+        int x = preorder[ind++];
         TreeNode root = new TreeNode(x);
-        int ind = -1;
-        for(int i = 0 ; i < il ; i++)
-            if(inorder[i] == x)
-            ind = i;
+        int pivot = mp.get(x);
 
-        
-        root.left = buildTree(Arrays.copyOfRange(preorder, 1, ind + 1), Arrays.copyOfRange(inorder, 0, ind));
-        root.right = buildTree(Arrays.copyOfRange(preorder, ind + 1, pl), Arrays.copyOfRange(inorder, ind + 1, il));
+        root.left = dfs(preorder, l, pivot - 1);
+        root.right = dfs(preorder, pivot + 1, r);
 
         return root;
     }
