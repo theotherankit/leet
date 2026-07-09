616638b1c2dea0606a1f2ea59ec79ecb5c8716d5
Ankit <ankitdey2004@gmail.com>
2026-07-09 14:09:21 +0530

    Time: 1 ms (51.83%) | Memory: 43.4 MB (65.55%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    HashMap<Integer, List<Character>> mp = new HashMap<>();
+    List<String> res = new ArrayList<>();
+    public List<String> letterCombinations(String digits) {
+        mp.put(2, List.of('a', 'b', 'c'));
+        mp.put(3, List.of('d', 'e', 'f'));
+        mp.put(4, List.of('g', 'h', 'i'));
+        mp.put(5, List.of('j', 'k', 'l'));
+        mp.put(6, List.of('m', 'n', 'o'));
+        mp.put(7, List.of('p', 'q', 'r', 's'));
+        mp.put(8, List.of('t', 'u', 'v'));
+        mp.put(9, List.of('w', 'x', 'y', 'z'));
+
+        String s = "";
+        dfs(digits, s, 0);
+        return res;
+    }
+
+    void dfs(String digits, String s, int start) {
+        if(digits.length() == start) {
+            res.add(s);
+            return;
+        }
+
+        int x = digits.charAt(start) - '0';
+        List<Character> letters = mp.get(x);
+        for(int i = 0 ; i < letters.size() ; i++)
+            dfs(digits, s + letters.get(i), start + 1);
+    }
+}
