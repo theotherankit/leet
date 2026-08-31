b6defcc6db0f7380761c6b7ad790aed733c5fb91
Ankit <ankitdey2004@gmail.com>
2026-08-31 17:38:12 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (19.17%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -10,7 +10,6 @@ class Solution {
         for(int i = 2 ; i < n ; i++)
             dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
 
-        System.out.println(Arrays.toString(dp));
         return dp[n - 1];
     }
 }
